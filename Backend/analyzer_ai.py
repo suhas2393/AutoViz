82f62db99689a12483cea3fb9181f26c7dd312f4
suhas2393 <srk@extremenetworks.com>
2025-03-28 00:02:46 +0530

    Removing unwanted code in BE for devices

--- Backend/analyzer_ai.py
+++ Backend/analyzer_ai.py
@@ -57,47 +57,6 @@ def analyze_alerts(data):
         "alert_types": df["alert_type"].unique().tolist() if "alert_type" in df else [],
     }
 
-# Function to analyze device data
-def analyze_devices(data):
-    devices = data.get("data", [])
-    
-    total_devices = len(devices)
-    connected_devices = sum(1 for d in devices if d.get("connected", False))
-    unconnected_devices = total_devices - connected_devices
-
-    device_types = list(set(d.get("product_type") for d in devices if d.get("product_type")))
-
-    locations = {}
-    for d in devices:
-        site = d.get("locations", {}).get("site", "Unknown")
-        locations[site] = locations.get(site, 0) + 1
-
-    healthy_clients_total = sum(d.get("healthy_clients", 0) for d in devices)
-    unhealthy_clients_total = sum(d.get("unhealthy_clients", 0) for d in devices)
-
-    firmware_versions = list(set(d.get("software_version") for d in devices if d.get("software_version")))
-
-    config_mismatch_count = sum(1 for d in devices if d.get("config_mismatch", False))
-
-    management_issues = {
-        "has_management_ip_issue": sum(1 for d in devices if d.get("has_management_ip_issue", False)),
-        "has_default_gateway_issue": sum(1 for d in devices if d.get("has_default_gateway_issue", False)),
-        "has_firmware_version_issue": sum(1 for d in devices if d.get("has_firmware_version_issue", False))
-    }
-
-    return {
-        "total_devices": total_devices,
-        "device_types": device_types,
-        "connected_devices": connected_devices,
-        "unconnected_devices": unconnected_devices,
-        "devices_per_location": locations,
-        "healthy_clients_total": healthy_clients_total,
-        "unhealthy_clients_total": unhealthy_clients_total,
-        "firmware_versions": firmware_versions,
-        "config_mismatch_count": config_mismatch_count,
-        "management_issues": management_issues
-    }
-
 # API to get analyzed client data
 @app.route('/analyze', methods=['GET'])
 def get_analyzed_data():
@@ -132,24 +91,6 @@ def get_analyzed_data():
     parsed_data = json.loads(response.choices[0].message.content[7:-4])
     return parsed_data
 
-# API to get raw clients data
-@app.route('/raw/clients', methods=['GET'])
-def get_raw_clients():
-    data = load_data("clients.json")
-    if not data:
-        return jsonify({"error": "Data not found"}), 404
-
-    return jsonify(data)
-
-# API to get analyzed alert data dynamically
-@app.route('/alerts', methods=['GET'])
-def get_alert_analysis():
-    data = load_data("alerts.json")
-    if not data:
-        return jsonify({"error": "Data not found"}), 404
-
-    analyzed_data = analyze_alerts(data)
-    return jsonify(analyzed_data)
 
 # API to get raw alerts data
 @app.route('/raw/alerts', methods=['GET'])
@@ -159,16 +100,6 @@ def get_raw_alerts():
         return jsonify({"error": "Data not found"}), 404
 
     return jsonify(data)
-
-# API to get analyzed device data
-@app.route('/devices', methods=['GET'])
-def get_device_analysis():
-    data = load_data("devices.json")
-    if not data:
-        return jsonify({"error": "Data not found"}), 404
-
-    analyzed_data = analyze_devices(data)
-    return jsonify(analyzed_data)
 
 # API to get raw devices data
 @app.route('/raw/devices', methods=['GET'])
@@ -181,3 +112,12 @@ def get_raw_devices():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+# API to get raw clients data
+@app.route('/raw/clients', methods=['GET'])
+def get_raw_clients():
+    data = load_data("clients.json")
+    if not data:
+        return jsonify({"error": "Data not found"}), 404
+
+    return jsonify(data)
